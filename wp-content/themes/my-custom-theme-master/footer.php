d75ac028cffe24ec054c648094c1a1a4cf71b297
gabriel da silva braga <6036046@mborijnland.nl>
2024-12-06 09:10:28 +0100

    update

--- wp-content/themes/my-custom-theme-master/footer.php
+++ wp-content/themes/my-custom-theme-master/footer.php
@@ -5,7 +5,19 @@
  * @copyright  Copyright (c) 2020, Danny Cooper
  * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
  */
-
-		wp_footer(); ?>
-	</body>
+        wp_footer(); ?>
+        <footer>
+    <div class="footer-logo">
+        <img src="http://wordpress_periode5_malik.local/wp-content/uploads/2024/10/Screenshot-2024-10-09-103009.png" alt="Logo">
+    </div>
+    <div class="footer-copyright">
+        <p>&copy; <?php echo date("Y"); ?> TechSphere Events All rights reserved.</p>
+    </div>
+    <div class="footer-social-media">
+        <a href="https://facebook.com" class="fa fa-facebook" target="_blank"></a>
+        <a href="https://twitter.com" class="fa fa-twitter" target="_blank"></a>
+        <a href="https://instagram.com" class="fa fa-instagram" target="_blank"></a>
+    </div>
+</footer>
+    </body>
 </html>
